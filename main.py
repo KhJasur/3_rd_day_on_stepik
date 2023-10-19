85e3c1b6ed1d04e21d0c98889e68e2ec1e448ff5
KhJasur <147963608+KhJasur@users.noreply.github.com>
2023-10-19 22:30:36 +0500

    В этот день я очень устал и не был в настроении писать код

---
+++ main.py
@@ -0,0 +1,37 @@
+# a = int(input())
+# if -1 < a < 17:
+#     print("Принадлежит")
+# else:
+#     print("Не принадлежит")
+
+
+# a = int(input())
+# if -2 < a < 7:
+#     print("Не Принадлежит")
+# else:
+#     print("Принадлежит")
+
+
+
+# a = int(input())
+#
+# if (-30 < a < -2) or (7 < a < 25):
+#     print("Принадлежит")
+# else:
+#     print("Не принадлежит")
+
+
+
+# a = int(input())
+#
+# if 1000 <= a <= 9999 and (a % 17 == 0 or a % 7 == 0):
+#     print("YES")
+# else:
+#     print("NO")
+
+
+
+
+
+
+
